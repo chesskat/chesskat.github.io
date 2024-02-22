e136649f1e33c44a118a3c31cdb59e95019a3149
chesskat <159969931+chesskat@users.noreply.github.com>
2024-02-22 15:56:29 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ document.getElementById('nav-container').innerHTML = `
 
     <nav>
         <a href="index.html">About</a>
-        <a href="processing.html" class="dropbtn">Kat's Processing.js Animations & Games</a>
+        <a href="processing.html" class="dropbtn">Animations & Games in JavaScript Processing</a>
         <a href="mailto:katarinajojones@gmail.com">Contact</a> 
     </nav>
 
