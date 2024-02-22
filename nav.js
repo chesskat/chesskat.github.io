00e450a180643eae2ebcefee1ce935d596a36c2c
chesskat <159969931+chesskat@users.noreply.github.com>
2024-02-22 15:49:35 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,26 +3,8 @@ document.getElementById('nav-container').innerHTML = `
 
     <nav>
         <a href="index.html">About</a>
-        <div class="dropdown">
-            <a href="processing.html" class="dropbtn">JS Animations</a>
-            <div class="dropdown-content">
-                <a href="animated_underwater_scene.html" >Underwater Scene</a>
-		<a href="barns.html" >Barnsley Fern animation (The Coding Train Challenge #108)</a>
-		<a href="bouncingball.html" >Bouncing Ball loading screen</a>
-  		<a href="rain.html">Digital Rain</a>
-		
-  
-  <!-- <a href="movablecircle.html" >Clickable circle</a>
-  		<a href="magic8ball.html" >Magic 8-Ball</a>
-		<a href="pong.html" >Pong game</a>
- //--> 
-            </div>
-
-     
-        </div>
+        <a href="processing.html" class="dropbtn">Kat's Processing.Js Projects</a>
         <a href="mailto:katarinajojones@gmail.com">Contact</a> 
-	
-
     </nav>
 
 `;
