093c6e6a28c8c168614307da126d23361785f3d2
chesskat <159969931+chesskat@users.noreply.github.com>
2024-02-22 12:45:14 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,6 +8,11 @@ document.getElementById('nav-container').innerHTML = `
             <div class="dropdown-content">
                 <a href="animated_underwater_scene.html" >Underwater Scene</a>
 		<a href="barns.html" >Barnsley Fern animation (The Coding Train Challenge #108)</a>
+		<a href="bouncingball.html" >Bouncing Ball loading screen</a>
+		<a href="movablecircle.html" >Clickable circle</a>
+  		<a href="magic8ball.html" >Magic 8-Ball</a>
+		<a href="pong.html" >Pong game</a>
+
             </div>
         </div>
         <a href="mailto:katarinajojones@gmail.com">Contact</a>
