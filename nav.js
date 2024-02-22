842444b3b3a5bcc302d85b86a6fbfc0364029f41
chesskat <159969931+chesskat@users.noreply.github.com>
2024-02-22 11:59:33 -0800

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,16 @@
+// nav.js
+document.getElementById('nav-container').innerHTML = `
+
+    <nav>
+        <a href="index.html">About</a>
+        <div class="dropdown">
+            <a href="processing.html" class="dropbtn">JS Animations</a>
+            <div class="dropdown-content">
+                <a href="animated_underwater_scene.html" >Underwater Scene</a>
+		<a href="barns.html" >Barnsley Fern animation</a>
+            </div>
+        </div>
+        <a href="mailto:katarinajojones@gmail.com">Contact</a>
+    </nav>
+
+`;
