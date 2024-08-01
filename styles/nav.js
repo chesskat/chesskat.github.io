98faf714cb878f7475d5f506c0cc209a2b105e26
Katarina Jones <159969931+chesskat@users.noreply.github.com>
2024-07-31 17:37:20 -0700

    Update nav.js

--- styles/nav.js
+++ styles/nav.js
@@ -3,6 +3,7 @@ document.getElementById('nav-container').innerHTML = `
 
     <nav>
         <a href="index.html">About</a>
+        <!-- <a href="blog/index.html">Blog</a> -->
         <a href="skills/programming/processing.html" class="dropbtn">Animations & Games</a>
         <a href="mailto:katarinajojones@gmail.com">Contact</a> 
     </nav>
