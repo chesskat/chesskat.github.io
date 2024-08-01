8425f07a0e73eed7b02a0f493541cd676dfdc0bb
Katarina Jones <159969931+chesskat@users.noreply.github.com>
2024-08-01 05:13:28 -0700

    Update nav.js

--- styles/nav.js
+++ styles/nav.js
@@ -1,9 +1,10 @@
 document.getElementById('nav-container').innerHTML = `
   <nav>
     <a href="../../../../../../../../index.html">About Me</a>
+    
     <div class="dropdown">
       <a href="../../../../../../../../skills/index.html" class="dropbtn">Skills</a>
-      <a href="../../../../../../../../Blog/index.html">Blog</a>
+
       <div class="dropdown-content">
         <a href="../../../../../../../../skills/programming/index.html" class="dropdown-submenu">Programming</a>
         <div class="dropdown-submenu-content">
@@ -13,6 +14,7 @@ document.getElementById('nav-container').innerHTML = `
         <a href="../../../../../../../../skills/chess.html">Chess</a>
       </div>
     </div>
+          <a href="../../../../../../../../Blog/index.html">Blog</a>
     <a href="mailto:katarinajojones@gmail.com">Contact</a>
   </nav>
 `;
