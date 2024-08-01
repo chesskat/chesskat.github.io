285bc202f8c297c1147062067e0d654d58a71827
Katarina Jones <159969931+chesskat@users.noreply.github.com>
2024-08-01 05:41:28 -0700

    Update nav.js

--- styles/nav.js
+++ styles/nav.js
@@ -2,18 +2,19 @@ document.getElementById('nav-container').innerHTML = `
   <nav>
     <a href="../../../../../../../../index.html">About Me</a>
     
-    <div class="dropdown">
-      <a href="../../../../../../../../skills/index.html" class="dropbtn">Skills</a>
-
-      <div class="dropdown-content">
-        <a href="../../../../../../../../skills/programming/index.html" class="dropdown-submenu">Programming</a>
-        <div class="dropdown-submenu-content">
-          <a href="../../../../../../../../skills/programming/processing.html">Animations & Games</a>
-          <a href="../../../../../../../../skills/programming/data-science.html">Data Science</a>
-        </div>
-        <a href="../../../../../../../../skills/chess.html">Chess</a>
+<div class="dropdown">
+  <a href="../../../../../../../../skills/index.html" class="dropbtn">Skills</a>
+  <div class="dropdown-content">
+    <div class="dropdown-submenu">
+      <a href="../../../../../../../../skills/programming/index.html">Programming</a>
+      <div class="dropdown-submenu-content">
+        <a href="../../../../../../../../skills/programming/processing.html">Animations & Games</a>
+        <a href="../../../../../../../../skills/programming/data-science.html">Data Science</a>
       </div>
     </div>
+    <a href="../../../../../../../../skills/chess.html">Chess</a>
+  </div>
+</div>
           <a href="../../../../../../../../blog/index.html">Blog</a>
     <a href="mailto:katarinajojones@gmail.com">Contact</a>
   </nav>
