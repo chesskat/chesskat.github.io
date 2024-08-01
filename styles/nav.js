af7cb5a5e9064a2bb9456c9c4115857312a6f60d
Katarina Jones <159969931+chesskat@users.noreply.github.com>
2024-07-31 22:38:01 -0700

    Update nav.js

--- styles/nav.js
+++ styles/nav.js
@@ -2,9 +2,9 @@
 document.getElementById('nav-container').innerHTML = `
 
     <nav>
-        <a href="chesskat.github.io/index.html">About</a>
+        <a href="../../../../chesskat.github.io/index.html">About</a>
         <!-- <a href="blog/index.html">Blog</a> -->
-        <a href="chesskat.github.io/skills/programming/processing.html" class="dropbtn">Animations & Games</a>
+        <a href="../../../../chesskat.github.io/skills/programming/processing.html" class="dropbtn">Animations & Games</a>
         <a href="mailto:katarinajojones@gmail.com">Contact</a> 
     </nav>
 
